40f0a34fc605d86d9512776acc1c4b38ba14b786
Shiv Kumar <shivkumar@airasia.com>
2023-05-28 10:43:54 +0530

    Initial Commit

---
+++ SwiftUI-Embedded-in-UIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftUI-Embedded-in-UIKit
+//
+//  Created by Shiv kumar on 28/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
